1bd11f1843798ba878e6cce706ba33f7d79ed832
nickshamrock <nickshamrock1997@gmail.com>
2024-05-10 19:19:42 +0300

    добавил счетчик символов без пробелов + провел тесты (подробнее в файле readme.md)

--- script.js
+++ script.js
@@ -15,18 +15,18 @@ formInputText.addEventListener('input', (event) => {
   let lengthOfText = formInputText.value; 
   result.textContent = lengthOfText.length;
   //счетчик в span для кол-ва символов без пробелов
-  let textWthSymbols = formInputText.value;
-  let trimmedText = textWthSymbols.trim()
-  resultWthSymbols.textContent = trimmedText.length;
+  resultWthSymbols.textContent = lengthOfText.replace(/\s+/gu, '').length;
 });
 
-//команды для проверки и отладки 
-// нужно использовать метод trim для подсчета количества символов без пробелов 
-// нужно использовать свойство length для подсчета количества символов с пробелами или без пробелов 
+
+console.log()
+
 /* 
 
 документацию и полезные советы можно и нужно взять из этой статьи на Доке 
 
 https://doka.guide/js/string-length/#:~:text=%D0%A1%D0%B2%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE%20length%20%D1%85%D1%80%D0%B0%D0%BD%D0%B8%D1%82%20%D0%B4%D0%BB%D0%B8%D0%BD%D1%83%20%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8,%D0%94%D0%BB%D0%B8%D0%BD%D0%B0%20%D0%BF%D1%83%D1%81%D1%82%D0%BE%D0%B9%20%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8%20%D1%80%D0%B0%D0%B2%D0%BD%D0%B0%200.
+
+можно также использовать метод split
 
 */
