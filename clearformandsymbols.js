67c91d8ac250c18ff82f2626d55c79ded4496a6d
nickshamrock <nickshamrock1997@gmail.com>
2024-05-12 18:00:11 +0300

    убрал лишние пробелы

--- clearformandsymbols.js
+++ clearformandsymbols.js
@@ -2,10 +2,8 @@ let buttonClear = document.getElementById('button-clear');
 
 buttonClear.addEventListener('click', function (event) { 
     event.preventDefault(); 
-    
     result.textContent = '0';
     resultWthSymbols.textContent = 0; 
     formInputText.value = '';
     priceForText.textContent = 'кликните на «Посчитать»'
-
 }); 
